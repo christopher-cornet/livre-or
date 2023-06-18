b2e365a07d5cea4aef9d6067d62b218bb0db2790
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-06-18 23:17:17 +0200

    Add files via upload

--- classes/Database.php
+++ classes/Database.php
@@ -2,7 +2,28 @@
 
 class Database {
     public function __construct() {
-        $db = new PDO ('mysql:host=localhost; dbname=livreor', 'root', '');
+        $username = 'root';
+        $password = '';
+        $db = new PDO ('mysql:host=localhost; dbname=livreor', $username, $password);
+    }
+
+    public function modify_login_password() {
+
+    }
+
+    public function modify_login() {
+        $user_login = $_POST['user_login'];
+        $sql = "Select password FROM user WHERE login = '$user_login'";
+        $sql2 = "UPDATE login SET login = '$user_login' WHERE login = '$user_login'";
+        $db = new PDO ('mysql:host=localhost; dbname=livreor', $username, $password);
+        $query = $db->prepare($sql);
+        $query->execute(array($user_login));
+        $query2 = $db->prepare($sql2);
+        $query2->execute(array($user_login));
+    }
+
+    public function modify_password() {
+
     }
 }
 
--- php/profil.php
+++ php/profil.php
@@ -1,24 +1,41 @@
 <?php
-require('../classes/Database.php');
+include('../classes/Database.php');
 include('../classes/User.php');
 
 session_start();
 
-error_reporting(0);
+// error_reporting(0);
 
 if (!empty($_SESSION['user'])) {
     $name = $_SESSION['user']; 
 }
 
 if (isset($_POST['modify'])) {
+    // If the 2 entries are not empty: modify the users data in the database
     if (!empty($_POST['user_login']) && !empty($_POST['password'])) {
         $user_login = $_POST['user_login'];
         $password = $_POST['password'];
         $sql = "SELECT login, password FROM user WHERE login = '$user_login' AND password = ?";
         $query = "UPDATE user SET login = '$user_login', password = '$password' WHERE login = '$user_login'";
-        $db->query($query);
+        
+
+        // "Select password FROM user WHERE login = '$user_login'";
+
+        // $db->query($query);
         // $query = $db->prepare($sql);
         // $query->execute(array($user_login, $password));
+    }
+    // If password is empty: modify the user login in the database
+    elseif (!empty($_POST['user_login']) && empty($_POST['password'])) {
+        // Méthode
+        $database->modify_login();
+    }
+    // If the user login is empty: modify the password in the database
+    elseif (empty($_POST['user_login']) && !empty($_POST['password'])) {
+        // Méthode
+    }
+    else {
+        echo "Aucune information à modifier.";
     }
 }
 
@@ -48,8 +65,8 @@ if (isset($_POST['modify'])) {
     <main>
         <h1>Bienvenue <?php if ($_SESSION['user'] == false) {echo "utilisateur Anonyme"; } else {echo $name;}?> !</h1>
         <form action="" method="post">
-            <input type="text" placeholder="<?php if ($_SESSION['user'] == true) {echo $name;} else {echo "Nom d'utilisateur";} ?>" name="user_login" required>
-            <input type="password" placeholder="Mot de passe" name="password" required>
+            <input type="text" placeholder="<?php if ($_SESSION['user'] == true) {echo $name;} else {echo "Nom d'utilisateur";} ?>" name="user_login">
+            <input type="password" placeholder="Mot de passe" name="password">
             <input class="modify" type="submit" name="modify" value="Modifier">
         </form>
     </main>
